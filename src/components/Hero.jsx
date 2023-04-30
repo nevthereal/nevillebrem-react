57579f2b3bac24e255446c0d620a318546e0b555
Neville Brem <contact@nevillebrem.com>
2023-04-30 11:27:03 +0200

    undo hero overwork

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className='h-screen px-4 text-white wave-white'>
         <div className='mt-[-6rem] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='md:text-lg mx-auto font-bold font-mono uppercase text-nevBlue'><span className='text-yellow-500'>print</span><span className='text-pink-500'>(</span>'Hello World my name is'<span className='text-pink-500'>)</span></p>
+            <p className='md:text-lg mx-auto font-bold font-mono uppercase text-nevBlue'>Hello World, my name is</p>
             <p className='text-5xl md:text-6xl font-bold'>Neville Brem</p>
             <p className='text-gray-400 font-bold text-lg md:text-xl'>I am a <Typed strings={['Web Developer', 'Video Editor', 'Photoshop Editor', 'Digital Artist']} loop typeSpeed={80} backSpeed={60} /></p>
             <motion.button initial={{ scale: 0, rotate: 180 }} animate={{ rotate: 0, scale: 1 }} transition={{ type: "spring", stiffness: 260, damping: 20, duration: 500}} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className="rounded-3xl font-bold bg-nevBlue mx-auto py-2 px-4 my-4 md:text-lg shadow-nevBlue/30 shadow-lg"><a href="#about">Let's Start!</a></motion.button>
