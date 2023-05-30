b181205cb127a73aae7af8c8eb9ed76c74da3004
Neville Brem <contact@nevillebrem.com>
2023-05-30 06:23:27 +0200

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
-import envCompatible from 'vite-plugin-env-compatible'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  envPrefix: 'REACT_APP_',
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
@@ -14,7 +11,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    envCompatible(),
     svgrPlugin({
       svgrOptions: {
         icon: true,
