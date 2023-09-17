0108a1292e1b60fafc748ba0095b30003af38e94
Neville Brem <contact@nevillebrem.com>
2023-09-17 19:14:13 +0200

    changed social style

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,6 +4,7 @@ import { faVideo } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const style = {
+  card: `card p-4 my-4 md:my-0`,
   icon: `text-4xl hover:bg-gray-200 border-2 hover:border-gray-300 p-2 aspect-square rounded-full duration-200 border-gray-100 hover:animate-pulse`,
 };
 
@@ -13,8 +14,8 @@ const Social = () => {
       <h1 className='font-[Montserrat] font-bold text-white pb-16'>
         <FontAwesomeIcon icon={faVideo} /> SOCIAL
       </h1>
-      <div className='grid md:grid-cols-2 grid-rows-2 md:grid-rows-none gap-16 md:mx-16'>
-        <div className='card p-4'>
+      <div className='md:grid md:grid-cols-2 md:gap-4 md:mx-20'>
+        <div className={style.card}>
           <div className='flex justify-between'>
             <h2 className='font-[Montserrat] font-bold'>Instagram</h2>
             <a href='https://instagram.com/nevillebrem' target='_blank'>
@@ -26,7 +27,7 @@ const Social = () => {
             some pictures!
           </p>
         </div>
-        <div className='card p-4'>
+        <div className={style.card}>
           <div className='flex justify-between'>
             <h2 className='font-[Montserrat] font-bold'>Twitter</h2>
             <a href='https://twitter.com/BremNeville' target='_blank'>
