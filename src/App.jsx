b3c743ecd47cb5295a5f70a540628679302acad5
Neville Brem <bremneville@gmail.com>
2024-08-01 18:28:11 +0200

    removed contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Social from "./components/Social";
 
 function App() {
   return (
@@ -17,7 +15,6 @@ function App() {
       <About />
       <Skills />
       <Projects />
-      <Contact />
       <Footer />
     </div>
   );
